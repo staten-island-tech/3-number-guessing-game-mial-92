f460c8399d73364fdf0d8478ea37726ec48a5058
mial-92 <mialin0170@gmail.com>
2025-09-25 07:12:23 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-number = 10
+""" number = 10
 while number != 0:
     print(number)
     number = number - 1
+     """
+
+import random
+RandomNumber = random.randint(1 , 10)
+guess = False
+print(input("Guess a number from 1 to 10"))
+
+
+ 
